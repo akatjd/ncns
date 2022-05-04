31dfea8dc1433d3ec9645da8eb916d349b574998
KMS <KMS@DESKTOP-7NKSLEO>
2022-05-04 16:39:05 +0900

    first setting

---
+++ nscs/src/test/java/com/nscs/NscsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nscs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NscsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
